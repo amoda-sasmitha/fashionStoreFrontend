c13cebdcfc4f4cc93687d0336b4f5b7b40e95167
ozandabb <osanda.gamage.79@gmail.com>
2020-04-17 22:45:44 +0530

    Offer user page dummy desgin complete

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,26 @@
-
-import Home from '../views/home';
-import Category from '../views/products/category';
+import Home from "../views/home";
+import Category from "../views/products/category";
+import Offers from "../views/offers/offersMain";
 
 let indexRoutes = [
-    {
-      path: "/",
-      name: "Home",
-      component: Home,
-      exact: true,
-    },
-    {
-      path: "/categories",
-      name: "Category",
-      component: Category,
-      exact: true,
-    }
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    exact: true,
+  },
+  {
+    path: "/categories",
+    name: "Category",
+    component: Category,
+    exact: true,
+  },
+  {
+    path: "/offers",
+    name: "Offers",
+    component: Offers,
+    exact: true,
+  },
 ];
 
 export default indexRoutes;
