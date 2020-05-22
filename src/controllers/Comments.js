5fd118e2cac6d4c9a2a7137c269caa8224d2536a
ozandabb <osanda.gamage.79@gmail.com>
2020-05-22 13:53:54 +0530

    Admin comment section designed

--- src/controllers/Comments.js
+++ src/controllers/Comments.js
@@ -2,14 +2,6 @@ import axios from "axios";
 import Config from "./Config";
 
 export const insertComment = (data) => {
-  //   let formdata = new FormData();
-  //   formdata.set("userid", data.userId);
-  //   formdata.set("username", data.username);
-  //   formdata.set("produtid", data.produtid);
-  //   formdata.set("comment", data.comment);
-  //   formdata.set("rating", data.ratings);
-  //   formdata.set("created_at", data.Date);
-
   return new Promise((resolve, reject) => {
     return axios
       .post(`${Config.host}${Config.port}/comment/insert`, data)
@@ -25,7 +17,7 @@ export const insertComment = (data) => {
 export const getAllComments = () => {
   return new Promise((resolve, reject) => {
     return axios
-      .get(`${Config.host}${Config.port}/comment/getall`)
+      .get(`${Config.host}${Config.port}/comment/GetAll`)
       .then((result) => {
         if (result.data.code == 200) {
           resolve(result.data.data);
@@ -35,6 +27,35 @@ export const getAllComments = () => {
       })
       .catch((err) => {
         reject(err);
+      });
+  });
+};
+export const getCommentByProductId = (id) => {
+  return new Promise((resolve, reject) => {
+    return axios
+      .get(`${Config.host}${Config.port}/comment/GetComByProId/${id}`)
+      .then((result) => {
+        if (result.data.code == 200) {
+          resolve(result.data.data);
+        } else {
+          resolve([]);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export const deleteComment = (id) => {
+  return new Promise((resolve, reject) => {
+    return axios
+      .delete(`${Config.host}${Config.port}/comment/delete/${id}`)
+      .then((result) => {
+        resolve({ code: 200, message: result.data.message });
+      })
+      .catch((err) => {
+        reject({ code: 0, error: err });
       });
   });
 };
